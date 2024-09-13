765c222f027bf59fd1372b440d3467267a501bc5
Nicolas <s2518809@ed.ac.uk>
2024-09-13 02:18:38 +0100

    constituency based predicate extraction

--- FullParser/ClauseParser.py
+++ FullParser/ClauseParser.py
@@ -12,7 +12,6 @@ class ClauseParser():
       just as clause_type: None or clause: None
       """
     def __init__(self):
-        self.nlp = spacy.load('en_core_web_md')
         return
 
     def get_sentence(self,span):
@@ -22,22 +21,23 @@ class ClauseParser():
         else:
             return self.get_sentence(parent)
 
-    def get_pred_info(self,pred_str):
-        final_pred = []
-        doc = self.nlp(pred_str)
-    
-        #information for each token, from spacy
-        token_annots = [{'str': token.text, 'lemma': token.lemma_, 'POS': token.pos_} for token in doc]
-        pos_tags = [token.pos_ for token in doc]
-    
-        #we only want verbs, prepositions, adjectives, and (only if the predicate contains an adjective) auxiliaries
-        for token in token_annots:
-            pos = token['POS']
-            if pos in ['VERB', 'ADP', 'ADJ'] or ("ADJ" in pos_tags and pos == "AUX"):
-                final_pred.append(token)
     
         return final_pred
-    def get_predicate(self,span):
+        
+    def flatten(self,xss):
+        return [x for xs in xss for x in xs]
+    
+    def get_last_predicate_tokens(self,span,sbar):
+        if span == sbar:
+            return []
+        tokens = []
+        for token in span:
+            if 'SBAR' in token._.parent._.labels:
+                return tokens
+            tokens.append(token)
+        return tokens
+    
+    def get_predicate(self,VP_span,sbar_span):
         """Returns the list of dictionaries of the embedding predicate token data of an SBAR span.
         The function assumes that the span is an embedded clause.
         :span: spacy.tokens.span.span
@@ -47,9 +47,10 @@ class ClauseParser():
             lemma: represents the token lemma if the string is modulated by tense
             POS: the part-of-speech tag for the lexical token
         """
+        pred_tokens = self.flatten([[t for t in child] for child in list(VP_span._.children)[:-1] if not any([bad_label in child._.labels for bad_label in ['S','SBAR', 'NP', 'PP']])])+self.get_last_predicate_tokens(list(VP_span._.children)[-1],sbar_span)
         final_pred = []
-        token_annots = [{'str': token.text, 'lemma': token.lemma_, 'POS': token.pos_} for token in span]
-        pos_tags = [token.pos_ for token in span]
+        token_annots = [{'str': token.text, 'lemma': token.lemma_, 'POS': token.pos_} for token in pred_tokens]
+        pos_tags = [token.pos_ for token in pred_tokens]
 
         #we only want verbs, prepositions, adjectives, and (only if the predicate contains an adjective) auxiliaries
         for token in token_annots:
@@ -57,6 +58,20 @@ class ClauseParser():
             if pos in ['VERB', 'ADP', 'ADJ'] or ("ADJ" in pos_tags and pos == "AUX"):
                 final_pred.append(token)
         return final_pred
+        
+    def test_pred_string(self,span,sbar_idx = None):
+        if not sbar_idx:
+            sbar_idx = span[0].i+1
+        parent = span._.parent
+        if parent == None:
+            return span
+        parent_label = parent._.labels
+        if "VP" in parent_label:
+            return  self.get_sentence(span)[parent[0].i:sbar_idx]
+        elif "NP" in parent_label:
+            return span
+        else:
+            return self.test_pred_string(parent,sbar_idx)
         
     def VP_parent(self,span):
         """Finds the nearest parent label as either VP NP or none.
@@ -69,18 +84,16 @@ class ClauseParser():
                 arg2: spacy.tokens.span.span
                     The first (leftmost) child of the VP labeled span.
         """
-        sbar_idx = span[0].i
-        # if 'CONJ' in self.get_sentence(span)[sbar_idx-1].pos_:
-        #     return (False, None)
         # Get the parent span
         parent = span._.parent
         if parent == None:
             return (False,None)
         parent_label = parent._.labels
         if "VP" in parent_label:
+            sbar_idx = span[0].i
             predicate = self.get_predicate(self.get_sentence(span)[parent[0].i:sbar_idx+1])
             if len(predicate) == 1:
-                if predicate[0]['str']=='is':
+                if predicate[0]['lemma']=='be':
                     return (False,None)
             return (True,predicate)        
         elif "NP" in parent_label:
@@ -90,6 +103,22 @@ class ClauseParser():
             # No NP or VP parent. Check next parent.
             return self.VP_parent(parent)
 
+    
+    def VP_parent_simple(self,span):
+        # Get the parent span
+        parent = span._.parent
+        if parent == None:
+            return None
+        parent_label = parent._.labels
+        if "VP" in parent_label:
+            return parent        
+        elif "NP" in parent_label:
+            # Found NP parent
+            return None
+        else:
+            # No NP or VP parent. Check next parent.
+            return self.VP_parent_simple(parent)
+        
     def VP_parent_new(self,span):
         parent = span._.parent
         if parent == None:
@@ -104,7 +133,6 @@ class ClauseParser():
             return (False,None)
         else:
             return self.VP_parent_new(parent)
-            
 
     def get_SBAR_spans(self,span):
         """Returns a list of SBAR spans given sentence.
@@ -174,16 +202,17 @@ class ClauseParser():
         clauses = []
         
         for sbar in SBAR_spans:
-            parent = self.VP_parent(sbar)
+            # parent = self.VP_parent(sbar)
+            parent = self.VP_parent_simple(sbar)
             # parent = self.VP_parent_new(sbar)
             # first_word = str(list(sbar._.children)[0]).lower()
             first_word = str(sbar[0])
             if len(self.get_SBAR_spans(sbar)) > 0:
                 clauses += self.parse_SBAR_clause(sentence,sbar)
-            if not parent[0] or first_word in ['because', 'since', 'while', 'as']:
+            if not parent or first_word in ['because', 'since', 'while', 'as', 'until', 'for']:
                 continue
             clauses += [{'sentence': str(sentence),
-                        'predicate': parent[1],
+                        'predicate': self.get_predicate(parent,sbar),
                          'type': self.get_clause_type(sbar),
                          'clause' : str(sbar)
                          }]
